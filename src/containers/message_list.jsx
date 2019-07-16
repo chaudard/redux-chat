68773fad2f969ac548b661c81ce8ae0388047b45
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-16 21:59:23 +0200

    connect message_list container with redux

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import Message from './message';
 import MessageForm from './message_form';
+
 class MessageList extends Component {
-    static defaultProps = {
-        messages: [
-            {
-            "author":"anonymous92",
-            "content":"Hello world!",
-            "created_at":"2017-09-26T23:03:16.365Z"
-            },
-            {
-            "author":"anonymous77",
-            "content":"My name is anonymous77",
-            "created_at":"2017-09-26T23:03:21.194Z"
-            }
-        ]
-      }
     renderList = () => {
         return this.props.messages.map( (message) => <Message message={message} key={message.created_at}/>)
     }
@@ -28,4 +18,11 @@ class MessageList extends Component {
         )
     }
 }
-export default MessageList;
+
+function mapReduxStateToProps(reduxState) {
+    return {
+      messages: reduxState.messages
+    };
+}
+  
+export default connect(mapReduxStateToProps)(MessageList);
