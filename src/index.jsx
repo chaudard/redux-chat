5ea6b954e3bba81b5f8c7515696020919ba3525a
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-16 21:22:41 +0200

    add initialState (of redux) ; prompt to get the userName

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,13 @@ const reducers = combineReducers({
   changeMe: (state = null, action) => state
 });
 
+const initialState = {
+  messages: [],
+  channels: [ {name: "general"},{name: "react"},{name: "paris"} ],
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
 // render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
