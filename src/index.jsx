8900aad467496cc121085be02156a5a0ded81e2a
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-16 21:40:19 +0200

    add middleware redux-logger

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
 
 // internal modules
 import App from './components/app';
@@ -25,9 +26,11 @@ const initialState = {
   selectedChannel: {name: "general"}
 };
 
+const middlewares = applyMiddleware(logger);
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
