4460bc2b00fd8ada3d7a5ad79a5090a4c914b554
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-16 21:47:15 +0200

    add some messages in initial state (for a simple channel app)

--- src/index.jsx
+++ src/index.jsx
@@ -21,7 +21,18 @@ const reducers = combineReducers({
 });
 
 const initialState = {
-  messages: [],
+  messages: [
+    {
+      "author":"anonymous92",
+      "content":"Hello world!",
+      "created_at":"2017-09-26T23:03:16.365Z"
+    },
+    {
+      "author":"anonymous77",
+      "content":"My name is anonymous77",
+      "created_at":"2017-09-26T23:03:21.194Z"
+    }
+  ],
   channels: [ {name: "general"},{name: "react"},{name: "paris"} ],
   currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
   selectedChannel: {name: "general"}
