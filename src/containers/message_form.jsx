b1803a5fba7a431d27d4ed5382652e129b5b5386
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-17 07:49:06 +0200

    turn the message_form into a real form with the input as a controlled component (the handleChange update the REACT state)

--- src/containers/message_form.jsx
+++ src/containers/message_form.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 class MessageForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''}
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        // alert('A name was submitted: ' + this.state.value);
+        // event.preventDefault();
+    }
+
     render() {
         return (
             <div>
-                <input type="text"/>
-                <button>send</button>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                    message:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Send" />
+                </form>
             </div>
         )
     }
